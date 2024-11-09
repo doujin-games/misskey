834b3266ab5cf07ed549f735c5d456f35a65d4a5
ozelotdev <contact@ozelot.dev>
2024-11-09 22:30:17 +0900

    fix: isQuote を検証する前に isRenote を検証させる

--- packages/backend/src/core/NoteCreateService.ts
+++ packages/backend/src/core/NoteCreateService.ts
@@ -365,7 +365,7 @@ export class NoteCreateService implements OnApplicationShutdown {
 
 		const willCauseNotification = mentionedUsers.some(u => u.host === null)
 			|| (data.visibility === 'specified' && data.visibleUsers?.some(u => u.host === null))
-			|| data.reply?.userHost === null || (this.isQuote(data) && data.renote.userHost === null) || false;
+			|| data.reply?.userHost === null || (this.isRenote(data) && this.isQuote(data) && data.renote.userHost === null) || false;
 
 		if (user.host !== null && willCauseNotification) {
 			const userEntity = await this.usersRepository.findOneBy({ id: user.id });
